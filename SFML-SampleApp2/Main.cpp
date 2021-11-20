5417d2f061f524b547b4f6aa52fb92817b108950
DESKTOP-69B688E\user <stav12212@gmail.com>
2021-11-20 15:06:12 +0200

    Initial Commit

---
+++ SFML-SampleApp2/Main.cpp
@@ -0,0 +1,34 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        sf::CircleShape shape(50.f);
+        shape.setFillColor(sf::Color(150, 50, 250));
+        window.draw(shape);
+        // window.draw(...);
+
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+}
